56ec4c0b7460b2b5482c98d1d944d6da6d8c962e
kyoungjin <soccer910@naver.com>
2026-02-09 03:21:28 +0900

    Supabase 통합 및 UI 구성 요소를 통해 페이지 생성 기능 추가.동적라우팅

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,47 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { supabase } from "@/utils/supabase";
+// Shadcn UI
 import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+// CSS
 import styles from "./page.module.scss";
 
 function Home() {
   const router = useRouter();
 
+  // 페이지 생성 및 Supabase 연동
+  const onCreate = async () => {
+    // Supabase 데이터베이스 row 생성
+    const { error, status } = await supabase
+      .from("todos")
+      .insert([
+        {
+          title: "",
+          start_date: new Date(),
+          end_date: new Date(),
+          contents: [],
+        },
+      ])
+      .select();
+
+    if (error) {
+      console.log(error);
+    }
+    // 방금 생성한 TODOLIST의 ID 값으로 URL 파라미터 생성/변경 => Nextjs 동적 라우팅(Dynamic Routing)
+    const { data } = await supabase.from("todos").select();
+
+    if (status === 201) {
+      toast("페이지 생성 완료!", {
+        description: "새로운 투두리스가 생성 되었습니다.",
+      });
+
+      if (data) {
+        router.push(`/create/${data[data?.length - 1].id}`);
+      } else return;
+    }
+  };
   return (
     <div className={styles.container}>
       <div className={styles.container__onBoarding}>
@@ -21,7 +56,7 @@ function Home() {
         <Button
           variant={"outline"}
           className="w-full bg-transparent text-orange-500 border-orange-400  hover:bg-orange-50 hover:text-orange-500"
-          onClick={() => router.push("/create")}
+          onClick={onCreate}
         >
           Add New Page
         </Button>
